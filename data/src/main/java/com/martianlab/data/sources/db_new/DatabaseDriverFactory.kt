1bfa74524eb12e37c10da62570cd9f2e95cd1c9e
as <sarsyonov@gmail.com>
2020-10-25 16:30:43 +0300

    sqldelight recipe_entity.sq updated category_entity.sq added

---
+++ data/src/main/java/com/martianlab/data/sources/db_new/DatabaseDriverFactory.kt
@@ -0,0 +1,8 @@
+package com.martianlab.data.sources.db_new
+
+import android.content.Context
+import com.squareup.sqldelight.db.SqlDriver
+
+class DatabaseDriverFactory(private val context: Context) {
+
+}
